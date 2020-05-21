e84d5d53409e471acfd85a5ca920996624895646
Tyler Corley <herocheetah191@gmail.com>
2020-05-20 19:13:04 -0700

    offset bottom row

--- keyboards/centromere/keymaps/purpledream/keymap.c
+++ keyboards/centromere/keymaps/purpledream/keymap.c
@@ -15,11 +15,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // "KC_SLSH","KC_ENT","KC_LCTL","KC_LGUI","MO(2)","MO(1)","KC_SPC","KC_RALT"],
   [_STD] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_ESC,    KC_A,    KC_S,    KC_D,LT(_ARW,KC_F),KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_ENT,\
+       KC_ESC,    KC_A,    KC_S,    KC_D,LT(_ARW,KC_F),KC_G,                        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_QUOT,\
+      KC_LSFT,    KC_QUOT, KC_Z,    KC_X,    KC_C,    KC_V,                         KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LCTL, KC_LGUI, MO(_FN),   MO(_NUM),  KC_SPC,  KC_RALT \
                                       //`--------------------------'  `--------------------------'
